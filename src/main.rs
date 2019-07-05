687430f3dec2d19a7059cbb5a20b16af6b47580c
Li Yazhou <me.ssword@gmail.com>
2019-07-05 11:58:31 +0800

    add load

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 const CHIP8_RAM: usize = 4096;
 const CHIP8_WIDTH: usize = 64;
 const CHIP8_HEIGHT: usize = 32;
 
 
 pub struct Chip8Machine {
-    ram: [u8, CHIP8_RAM],
+    pub ram: [u8; CHIP8_RAM],
     vram: [[u8; CHIP8_WIDTH]; CHIP8_HEIGHT],  // graphics memory
     vram_changed: bool,
     stack: [usize; 16],
@@ -22,7 +25,7 @@ pub struct Chip8Machine {
 
 impl Chip8Machine {
     pub fn new() -> Self {
-        let mut ram = [0u8; CHIP8_RAM];
+        let mut ram = [0; CHIP8_RAM];
 
         Self {
             vram: [[0; CHIP8_WIDTH]; CHIP8_HEIGHT],
@@ -34,16 +37,37 @@ impl Chip8Machine {
             pc: 0x200,
             sp: 0,
             keypad: [false; 16],
-            keypad_waiting: false,
+            keypad_waiting: 0,
             keypad_register: 0,
+            delay_timer: 0,
+            sound_timer: 0,
         }
     }
 
     pub fn load(&mut self, data: &[u8]) {
+        for (i, &byte) in data.iter().enumerate() {
+            let addr = 0x200 + i;
+            if addr >= 4096 {
+                break
+            }
+            self.ram[addr] = byte;
+        }
+    }
+}
+
+
+fn load_rom(path: &str, buf: &mut [u8; 4096]) -> usize {
+    let mut f = File::open(path).expect("file not found");
+    match f.read(buf) {
+        Err(_) => 0,
+        Ok(n) => n,
     }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut buf = [0u8; 4096];
+    load_rom("games/Airplane.ch8", &mut buf);
+    let mut machine = Chip8Machine::new();
+    machine.load(&buf);
 }
