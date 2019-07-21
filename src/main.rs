51e2903d491c92fb5f4a751a5f1803d63a75744d
Li Yazhou <me.ssword@gmail.com>
2019-07-21 17:35:29 +0800

    make the ui works

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ extern crate rand;
 mod vm;
 mod font;
 mod instruction;
+mod ui;
 
 use std::fs::File;
 use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 
 
 fn load_rom(path: &str, buf: &mut [u8; 4096]) -> usize {
@@ -18,7 +21,23 @@ fn load_rom(path: &str, buf: &mut [u8; 4096]) -> usize {
 
 fn main() {
     let mut buf = [0u8; 4096];
-    load_rom("games/Airplane.ch8", &mut buf);
     let mut vm = vm::VM::new();
+    let sdl_context = sdl2::init().unwrap();
+    let mut ui = ui::UI::new(sdl_context);
+
+    load_rom("games/Airplane.ch8", &mut buf);
     vm.load(&buf);
+
+    while let Ok(keypad) = ui.poll() {
+        let output = vm.step(keypad);
+        if output.vram_changed {
+            ui.draw(output.vram);
+        }
+
+        // let mut pixels = [[0u8; vm::CHIP8_WIDTH]; vm::CHIP8_HEIGHT];
+        // pixels[0][0] = 1;
+        // ui.draw(&pixels);
+
+        thread::sleep(Duration::from_millis(2));
+    }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,15 +1,19 @@
 
 use sdl2;
-use vm;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::pixels::Color;
+use super::vm::{CHIP8_HEIGHT, CHIP8_WIDTH};
 
-struct UI {
-    canvas: sdl2::Canvas<sdl2::Window>,
+pub struct UI {
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
+    events: sdl2::EventPump,
 }
 
-const SCALE_FACTOR: u32 = 20;
-const SCREEN_WIDTH: u32 = vm::CHIP8_WIDTH * SCALE_FACTOR;
-const SCREEN_HEIGHT: u32 = vm::CHIP8_HEIGHT * SCALE_FACTOR;
-
+const SCALE_FACTOR: u32 = 5;
+const SCREEN_WIDTH: u32 = CHIP8_WIDTH as u32 * SCALE_FACTOR;
+const SCREEN_HEIGHT: u32 = CHIP8_HEIGHT as u32 * SCALE_FACTOR;
 
 
 impl UI {
@@ -22,29 +26,77 @@ impl UI {
             .build()
             .unwrap();
         let mut canvas = window.into_canvas().build().unwrap();
-        canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
         canvas.present();
-        UI { canvas: canvas }
+        UI {
+            canvas: canvas,
+            events: sdl_context.event_pump().unwrap(),
+        }
     }
 
     pub fn draw(&mut self, pixels: &[[u8; CHIP8_WIDTH]; CHIP8_HEIGHT]) {
         for (y, row) in pixels.iter().enumerate() {
-            for (x, col) in row.iter().enumerate() {
+            for (x, &col) in row.iter().enumerate() {
                 let x = (x as u32) * SCALE_FACTOR;
                 let y = (y as u32) * SCALE_FACTOR;
 
-                self.canvas.set_draw_color(self.get_color(col));
-                self.canvas.fill_rect(Rect::new(x as i32, y as i32, SCALE_FACTOR, SCALE_FACTOR))
+                let c = self.get_color(col);
+                self.canvas.set_draw_color(c);
+                self.canvas.fill_rect(Rect::new(x as i32, y as i32, SCALE_FACTOR, SCALE_FACTOR));
             }
         }
+        self.canvas.present();
     }
 
-    fn get_color(&mut self, v: u8) -> sdl2::pixels::Color {
+    pub fn poll(&mut self) -> Result<[bool; 16], ()> {
+        let mut chip8_keys = [false; 16];
+
+        for event in self.events.poll_iter() {
+            if let Event::Quit { .. } = event {
+                return Err(());
+            }
+        }
+
+        let keys: Vec<Keycode> = self.events
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect();
+        
+        for key in keys {
+            let index = match key {
+                Keycode::Num1 => Some(0x1),
+                Keycode::Num2 => Some(0x2),
+                Keycode::Num3 => Some(0x3),
+                Keycode::Num4 => Some(0xc),
+                Keycode::Q => Some(0x4),
+                Keycode::W => Some(0x5),
+                Keycode::E => Some(0x6),
+                Keycode::R => Some(0xd),
+                Keycode::A => Some(0x7),
+                Keycode::S => Some(0x8),
+                Keycode::D => Some(0x9),
+                Keycode::F => Some(0xe),
+                Keycode::Z => Some(0xa),
+                Keycode::X => Some(0x0),
+                Keycode::C => Some(0xb),
+                Keycode::V => Some(0xf),
+                _ => None,
+            };
+
+            if let Some(i) = index {
+                chip8_keys[i] = true;
+            }
+        }
+        Ok(chip8_keys)
+    }
+
+    fn get_color(&mut self, v: u8) -> Color {
         if v == 0 {
-            sdl2::pixels::Color::RGB(0, 0, 0)
+            Color::RGB(0, 0, 0)
         } else {
-            sdl2::pixels::Color::RGB(0, 250, 0)
+            Color::RGB(0, 250, 0)
         }
     }
 }
