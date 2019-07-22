2521d68e6bc4a7b9817f42efd6d56ebb5277aa47
Li Yazhou <me.ssword@gmail.com>
2019-07-22 08:36:59 +0800

    mv to chip8rs

---
+++ src/chip8rs/mod.rs
@@ -0,0 +1,7 @@
+pub mod font;
+pub mod instruction;
+pub mod ui;
+pub mod vm;
+
+pub use vm::VM;
+pub use ui::UI;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 extern crate rand;
 
-mod vm;
-mod font;
-mod instruction;
-mod ui;
-
 use std::fs::File;
 use std::io::prelude::*;
 use std::thread;
 use std::time::Duration;
+mod chip8rs;
 
 
 fn load_rom(path: &str, buf: &mut [u8; 4096]) -> usize {
@@ -21,9 +17,9 @@ fn load_rom(path: &str, buf: &mut [u8; 4096]) -> usize {
 
 fn main() {
     let mut buf = [0u8; 4096];
-    let mut vm = vm::VM::new();
     let sdl_context = sdl2::init().unwrap();
-    let mut ui = ui::UI::new(sdl_context);
+    let mut vm = chip8rs::VM::new();
+    let mut ui = chip8rs::UI::new(sdl_context);
 
     load_rom("games/Airplane.ch8", &mut buf);
     vm.load(&buf);
@@ -33,10 +29,6 @@ fn main() {
         if output.vram_changed {
             ui.draw(output.vram);
         }
-
-        // let mut pixels = [[0u8; vm::CHIP8_WIDTH]; vm::CHIP8_HEIGHT];
-        // pixels[0][0] = 1;
-        // ui.draw(&pixels);
 
         thread::sleep(Duration::from_millis(2));
     }
